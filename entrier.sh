50b02b0984a2160379b05ab10f7719d3a1a68f3a
心言自由 <XinYaanZyoy@gmail.com>
2020-05-09 19:34:27 +0530

    Update entrier.sh

--- entrier.sh
+++ entrier.sh
@@ -5,10 +5,25 @@ DIR="arts";
 status=false;
 if [ -d "$DIR" ]; then
     git log -1 --name-only --oneline > changes
-    count=0
+    i=0
     while read -r line; do
-       printf '%d %s\n' "$count" "${line}"
+       printf '%d %s\n' "$i" "${line}"
        (( count++ ))
+       if [ "$i" -ne 0 ]; then
+           if [ -f "${line}" ]; then
+               if [ -f "_${line}" ]; then
+                   echo "IMPOSSIBLE!";
+               else
+                   echo "it was created!";
+               fi
+           else
+               if [ -f "_${line}" ]; then
+                   echo "it was deleted!";
+               else
+                   echo "IMPOSSIBLE!";
+               fi
+           fi
+       fi
     done < changes
 else
     echo "$DIR doesn't exist!";
