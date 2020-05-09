9c129d8c9cf1ae59bad6368dbba7925f1cbd6a28
心言自由 <XinYaanZyoy@gmail.com>
2020-05-09 19:43:40 +0530

    Update entrier.sh

--- entrier.sh
+++ entrier.sh
@@ -8,19 +8,17 @@ if [ -d "$DIR" ]; then
     i=0
     while read -r line; do
        printf '%d %s\n' "$i" "${line}"
-       if [ "$i" -ne 0 ]; then
-           if [ -f "${line}" ]; then
-               if [ -f "_${line}" ]; then
-                   echo "IMPOSSIBLE!";
-               else
-                   echo "it was created!";
-               fi
+       if [ -f "${line}" ]; then
+           if [ -f "_${line}" ]; then
+               echo "IMPOSSIBLE!";
            else
-               if [ -f "_${line}" ]; then
-                   echo "it was deleted!";
-               else
-                   echo "IMPOSSIBLE!";
-               fi
+               echo "it was created!";
+           fi
+       else
+           if [ -f "_${line}" ]; then
+               echo "it was deleted!";
+           else
+               echo "IMPOSSIBLE!";
            fi
        fi
     (( count++ ))
